8595e4e6b73aa1ddf076ecaae6a102408c8a7535
Matt <matthewjosefcruz@gmail.com>
2024-06-21 22:45:38 +0800

    Create GetWidth And GetHeight Functions For Different Ad Types

--- script.js
+++ script.js
@@ -22,9 +22,46 @@ const game = (() => {
     }
 
     //ad text options: 10 variations
-    const adText = ['Buy Now!', 'Click Me!', 'Free Money', 'Send Help pls, there is an armed intruder...', 'Women in the area', 'Dogs in the area', 'Free Ram Upgrades', 'Hotel Travel!!!', '90% Discount', 'Tap Me :) or else...'];
-    //ad style option: 5 variations
-    const adStyles = ['normal'];
+    const adText = ['Buy Now!', 'Click Me!', 'Free Money', 'Send Help pls, there is an armed intruder...', 'Women in the area', 'Dogs in the area', 'Free Ram Upgrades', 'Hotel Travel!!!', '90% Discount', 'Tap Me :) or else...', ':))))))'];
+
+    //ad style option: 5 variations (objects)
+    const adStyles = ['normal', 'big', 'small' ,'short', 'long'];
+    //necessary to find rem value
+    const rootCompStyles = window.getComputedStyle(document.querySelector('html'));
+    const rem = rootCompStyles.fontSize.replace(/\D/g, '');
+    //necessary to obtain values from css variables
+    const adContainerCompStyles = window.getComputedStyle(adContainer);
+    //returns computed width and heights of corresponding ad style
+    //calculates rem to pixels :)
+    function getHeight(style) {
+        //replace(), removes all non digit characters 
+        const short = adContainerCompStyles.getPropertyValue('--short-height').replace(/\D/g, '');
+        const medium = adContainerCompStyles.getPropertyValue('--medium-height').replace(/\D/g, '');
+        const long = adContainerCompStyles.getPropertyValue('--long-height').replace(/\D/g, '');
+        switch(style) {
+            // +4 to account for the borders (2px both sides)
+            case 'normal': return medium * rem + 4;
+            case 'big': return long * rem + 4;
+            case 'small':
+            case 'short': return short * rem + 4;
+            case 'long': return long * rem + 4;
+            default: console.log('bruh that is not a style');
+        }
+    }
+    function getWidth(style) {
+        const short = adContainerCompStyles.getPropertyValue('--short-width').replace(/\D/g, '');
+        const medium = adContainerCompStyles.getPropertyValue('--medium-width').replace(/\D/g, '');
+        const long = adContainerCompStyles.getPropertyValue('--long-width').replace(/\D/g, '');
+        switch(style) {
+            // +4 to account for the borders (2px both sides)
+            case 'normal': return medium * rem + 4;
+            case 'big': return long * rem + 4;
+            case 'small': return short * rem + 4;
+            case 'short': 
+            case 'long': return medium * rem + 4;
+            default: console.log('bruh that is not a style');
+        }
+    }
 
     //randomly creates the ad object, pushes it to the ads array
     function buildRandomAd() {
@@ -50,10 +87,11 @@ const game = (() => {
         return adStyles[randomMax(adStyles.length)];
     }
     //for top and left properties (position: absolute)
-    function randomLeft() {
+    //will account for different style width & heights to avoid going out of border
+    function randomLeft(style) {
 
     }
-    function randomTop() {
+    function randomTop(style) {
 
     }
 
